c8d1ec43c483175e2d4cf300d78f98b3a6027319
= <=>
2023-08-02 20:36:01 +0300

    js functionality

---
+++ script.js
@@ -0,0 +1,57 @@
+const apiKey = "f54605ffae5543bb9dcf32b27baa24d9"
+
+const recipeListEl = document.getElementById("recipe-list")
+
+
+
+function displayRecipes(recipes){
+    recipeListEl.innerHTML = ""
+    recipes.forEach((recipe) => {
+        const recipeItemEl = document.createElement("li")
+        recipeItemEl.classList.add("recipe-item")
+
+        const recipeImageEl = document.createElement("img")
+        recipeImageEl.src = recipe.image
+        recipeImageEl.alt = "recipe image"
+
+        const recipeTitleEl = document.createElement("h2")
+        recipeTitleEl.textContent = recipe.title
+
+        const recipeIngeredientsEl = document.createElement("p")
+        recipeIngeredientsEl.innerHTML = `<strong>Ingredients:</strong> ${
+            recipe.extendedIngredients.map((ingredient) => ingredient.original).join(", ")
+        }`
+
+        const recipeLinkEl = document.createElement("a")
+        recipeLinkEl.href = recipe.sourceUrl
+        recipeLinkEl.innerText = "view recipe"
+
+
+        recipeItemEl.appendChild(recipeImageEl)
+        recipeItemEl.appendChild(recipeTitleEl)
+        recipeItemEl.appendChild(recipeIngeredientsEl)
+        recipeItemEl.appendChild(recipeLinkEl)
+
+        recipeListEl.appendChild(recipeItemEl)
+
+    })
+}
+
+async function getRecipes(){
+    const url = `https://api.spoonacular.com/recipes/random?number=10&apiKey=${apiKey}`
+    const response = await fetch(url)
+
+    const data = await response.json()
+
+    return data.recipes
+}
+
+async function init(){
+    const recipes = await getRecipes()
+    // console.log(recipes)
+    displayRecipes(recipes)
+}
+
+init()
+
+
